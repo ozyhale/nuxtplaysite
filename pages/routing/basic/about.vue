e77298c76713c37eeb5bc98076cc0e24cb3e7323
Ozy Hale <ozyhale@gmail.com>
2020-02-24 11:48:32 +0800

    Exploring Routing

---
+++ pages/routing/basic/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    Routing > Basic > About
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/routing/dynamic/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    {{breadcrumbs}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data (){
+    return {
+      breadcrumbs: this.$route.path.replace(/\//g, " > ")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
